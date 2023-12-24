0950c3567a1ee1359f4c14f99469ffb288ffca83
omah-hub <orjichioma422@yahoo.com>
2023-12-23 21:07:38 -0800

    client and server

---
+++ client/scripts/upload.js
@@ -0,0 +1,53 @@
+document.addEventListener('DOMContentLoaded', function () {
+  var dropArea = document.getElementById('dropArea');
+  var fileInput = document.getElementById('image');
+
+  // Prevent the default behavior of drag-and-drop
+  dropArea.addEventListener('dragover', function (e) {
+    e.preventDefault();
+    dropArea.style.border = '2px dashed #aaa';
+  });
+
+  dropArea.addEventListener('dragleave', function () {
+    dropArea.style.border = '2px dashed #ccc';
+  });
+
+  // Handle the dropped files
+  dropArea.addEventListener('drop', function (e) {
+    e.preventDefault();
+    dropArea.style.border = '2px dashed #ccc';
+
+    var files = e.dataTransfer.files;
+    fileInput.files = files;
+  });
+});
+
+function submitForm() {
+  var form = document.getElementById('uploadForm');
+  var formData = new FormData(form);
+
+  // Use fetch to send the form data to the server
+  fetch('', {   //something should be here
+    method: 'POST',
+    body: formData
+  })
+  .then(response => response.json())
+  .then(data => {
+    // Handle the response from the server
+    handleResponse(data);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+}
+
+function handleResponse(data) {
+  // Display success or error message to the user
+  var messageElement = document.getElementById('message');
+  if (data.success) {
+    messageElement.textContent = 'Form submitted successfully!';
+    // Optionally, clear the form or perform other actions
+  } else {
+    messageElement.textContent = 'Error submitting form. Please try again.';
+  }
+}
---
+++ server/app.js
@@ -0,0 +1,88 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const multer = require('multer');
+const path = require('path');
+const mysql = require('mysql');
+const morgan = require('morgan')
+// require('dotenv').config()
+const cors = require('cors');
+const uuid = require('uuid');
+const db = require('./db');
+
+
+const app = express();
+const port = 3000;
+
+// Set up MySQL connection
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to MySQL:', err);
+    } else {
+        console.log('Connected to MySQL');
+    }
+});
+
+// Set up body-parser to parse form data
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Set up multer to handle file uploads
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + path.extname(file.originalname));
+    },
+});
+
+const upload = multer({ storage: storage });
+
+// Serve HTML form
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Handle form submission
+app.post('/process_form', upload.single('image'), (req, res) => {
+    // Access form data using req.body
+    const formData = req.body;
+
+    // Access uploaded file information using req.file
+    const imagePath = req.file ? req.file.path : null;
+
+    // Insert data into MySQL database
+    const sql = 'INSERT INTO properties (name, address, unit, city, state, room_type, price, description, image_path) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)';
+    const values = [
+        formData.name,
+        formData.address,
+        formData.unit,
+        formData.selectedCity,
+        formData.selectedState,
+        formData.selectedRoom,
+        formData.price,
+        formData.description,
+        imagePath,
+    ];
+
+    connection.query(sql, values, (error, results) => {
+        if (error) {
+            console.error('Error inserting data into MySQL:', error);
+            res.json({ success: false, message: 'Error submitting form' });
+        } else {
+            console.log('Data inserted into MySQL:', results);
+            res.json({ success: true, message: 'Form submitted successfully!' });
+        }
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
