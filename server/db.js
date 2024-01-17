e7c6eb6d4cc7d6d939c581d4139e7effd063b875
omah-hub <orjichioma422@yahoo.com>
2024-01-17 14:16:21 -0800

    my commit

--- server/db.js
+++ server/db.js
@@ -4,7 +4,10 @@ const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD
+    password: process.env.DB_PASSWORD,
+    ssl: {
+        rejectUnauthorized: false,
+    }
 })
 
 module.exports = pool.promise()
